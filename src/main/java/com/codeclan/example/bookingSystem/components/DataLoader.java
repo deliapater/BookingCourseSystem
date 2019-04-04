a81caf0a34fa04c9e57f52c96f7dbf6cf4b4cb45
delia paternina <delia.paternina@gmail.com>
2019-04-04 11:00:02 +0100

    controllers and data loader added

--- src/main/java/com/codeclan/example/bookingSystem/components/DataLoader.java
+++ src/main/java/com/codeclan/example/bookingSystem/components/DataLoader.java
@@ -1,4 +1,26 @@
 package com.codeclan.example.bookingSystem.components;
 
-public class DataLoader {
+
+import com.codeclan.example.bookingSystem.models.Course;
+import com.codeclan.example.bookingSystem.repositories.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+
+    @Autowired
+    CourseRepository courseRepository;
+
+    public DataLoader(){
+
+    }
+
+
+    public void run(ApplicationArguments args) {
+        Course course1 = new Course("Java", "Glasgow", 5);
+        courseRepository.save(course1);
+    }
 }
--- src/main/java/com/codeclan/example/bookingSystem/controllers/CourseController.java
+++ src/main/java/com/codeclan/example/bookingSystem/controllers/CourseController.java
@@ -1,4 +1,14 @@
 package com.codeclan.example.bookingSystem.controllers;
 
+import com.codeclan.example.bookingSystem.repositories.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(name= "/courses")
 public class CourseController {
+
+    @Autowired
+    CourseRepository courseRepository;
 }
