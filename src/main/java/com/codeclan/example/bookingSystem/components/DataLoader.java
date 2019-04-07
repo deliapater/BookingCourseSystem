d7d3c898c2e1827a28b32f4c1757dde8c85d7028
delia paternina <delia.paternina@gmail.com>
2019-04-07 13:49:23 +0100

    more data added

--- src/main/java/com/codeclan/example/bookingSystem/components/DataLoader.java
+++ src/main/java/com/codeclan/example/bookingSystem/components/DataLoader.java
@@ -33,16 +33,37 @@ public class DataLoader implements ApplicationRunner {
         Course course1 = new Course("Java", "Glasgow", 5);
         courseRepository.save(course1);
 
+        Course course2 = new Course("Photoshop", "Glasgow", 5);
+        courseRepository.save(course2);
+
         Customer customer1 = new Customer("Delia", "Glasgow", 25);
         customerRepository.save(customer1);
 
         Customer customer2 = new Customer("Cleyra", "Glasgow", 29);
         customerRepository.save(customer2);
 
+        Customer customer3 = new Customer("Jose", "Glasgow", 32);
+        customerRepository.save(customer3);
+
+        Customer customer4 = new Customer("Juan", "Glasgow", 28);
+        customerRepository.save(customer4);
+
+        Customer customer5 = new Customer("Maria", "Glasgow", 32);
+        customerRepository.save(customer5);
+
         Booking booking1 = new Booking("24-05-2019", course1, customer1);
         bookingRepository.save(booking1);
 
         Booking booking2 = new Booking("24-05-2019", course1, customer2);
         bookingRepository.save(booking2);
+
+        Booking booking3 = new Booking("24-05-2019", course1, customer3);
+        bookingRepository.save(booking3);
+
+        Booking booking4 = new Booking("24-05-2019", course1, customer4);
+        bookingRepository.save(booking4);
+
+        Booking booking5 = new Booking("24-05-2019", course2, customer5);
+        bookingRepository.save(booking5);
     }
 }
--- src/test/java/com/codeclan/example/bookingSystem/BookingSystemApplicationTests.java
+++ src/test/java/com/codeclan/example/bookingSystem/BookingSystemApplicationTests.java
@@ -38,14 +38,30 @@ public class BookingSystemApplicationTests {
 	public void createABookingThatHasACourseAndACustomer(){
 		Course course = new Course("Java", "Glasgow", 5);
 		courseRepository.save(course);
+		Course course2 = new Course("Photoshop", "Glasgow", 5);
+		courseRepository.save(course2);
+
 		Customer customer1 = new Customer("Delia", "Glasgow", 25);
 		customerRepository.save(customer1);
 		Customer customer2 = new Customer("Cleyra", "Glasgow", 29);
 		customerRepository.save(customer2);
+		Customer customer3 = new Customer("Jose", "Glasgow", 32);
+		customerRepository.save(customer3);
+		Customer customer4 = new Customer("Juan", "Glasgow", 28);
+		customerRepository.save(customer4);
+		Customer customer5 = new Customer("Maria", "Glasgow", 32);
+		customerRepository.save(customer5);
+
 		Booking booking1 = new Booking("24-05-2019", course, customer1);
 		bookingRepository.save(booking1);
 		Booking booking2 = new Booking("24-05-2019", course, customer2);
 		bookingRepository.save(booking2);
+		Booking booking3 = new Booking("24-05-2019", course, customer3);
+		bookingRepository.save(booking3);
+		Booking booking4 = new Booking("24-05-2019", course, customer4);
+		bookingRepository.save(booking4);
+		Booking booking5 = new Booking("24-05-2019", course2, customer5);
+		bookingRepository.save(booking5);
 	}
 
 //	@Test
